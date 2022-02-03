088db949bb59c88f0e7e1e3dd9b39305ef7e9636
Deepan Kumar <deepanrvdvenu@gmail.com>
2022-02-04 03:34:15 +0530

    first commit

---
+++ src/contracts/MemoryToken.sol
@@ -0,0 +1,54 @@
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MemoryToken is ERC721, ERC721Enumerable ,ERC721URIStorage  {
+  
+
+  constructor() ERC721('Memory Token', 'MEMORY'){}
+
+  function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+    function mint(address _to,string memory _tokenURI) public returns(bool) {
+       uint _tokenId = totalSupply() + 1;
+       _mint(_to, _tokenId);
+       _setTokenURI(_tokenId, _tokenURI);
+       return true;
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
+
+  
+
+  
+
+
+
+
